6c6e4291f2e27cc394d16501a458157dfe3d02b1
Brady Peterson <bpeterson1663@gmail.com>
2021-03-01 01:56:16 -0600

    feat(mongo driver): install and connect to mongo atlas

--- drivers/shell-to-driver-01-starting-code/backend/app.js
+++ drivers/shell-to-driver-01-starting-code/backend/app.js
@@ -1,13 +1,13 @@
 const path = require('path');
+require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient;
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const productRoutes = require('./routes/products');
 const authRoutes = require('./routes/auth');
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use('/images', express.static(path.join('backend/images')));
 
@@ -24,5 +24,15 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/', authRoutes);
+
+const uri = "mongodb+srv://daily-user:"+process.env.MONGO_PASSWORD+"@cluster0.ybtum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  console.log("ERROR", err)
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+})
 
 app.listen(3100);
